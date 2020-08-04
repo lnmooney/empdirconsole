817eec9ed2cfbabd7ed7eafe59ac4c1b1e1eede2
Lnmooney <lnmooney@comcast.net>
2020-08-04 13:12:45 -0400

    organize with functions instead of coding through

--- Program.cs
+++ Program.cs
@@ -10,69 +10,93 @@ namespace EFCoreApp
         {
             using (var context = new EFCoreAppContext())
             {
+
+            /**
+            * addEmpData function will prompt user to enter new data for the db
+            */
+            void addEmpData()
+            {
+                Console.WriteLine("Enter First Name :");
+                string firstNameEntered = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name :");
+                string lastNameEntered = Console.ReadLine();
+
+                Console.WriteLine("Enter Job Title :");
+                string jobTitleEntered = Console.ReadLine();
+
+                    var employee = new Employee {
+                        firstName = firstNameEntered,
+                        lastName = lastNameEntered,
+                        jobTitle = jobTitleEntered
+                    };
+                    context.Add(employee);
+                    context.SaveChanges();
+                    Console.WriteLine("You have successfully added employee data");
+            }
+
+            /**
+            * updateEmpData function will prompt user which employee record to edit by obtaining empId.
+            * It will then prompt user to enter updated employee record data for the db
+            */
+            void updateEmpData()
+            {
+                Console.WriteLine("Enter Employee ID :");
+                int empIdEntered = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Updated First Name :");
+                string firstNameEntered = Console.ReadLine();
+
+                Console.WriteLine("Enter Updated Last Name :");
+                string lastNameEntered = Console.ReadLine();
+
+                Console.WriteLine("Enter Updated Job Title :");
+                string jobTitleEntered = Console.ReadLine();
+
+                    var employee = new Employee { empId = empIdEntered };
+                    context.Entry(employee).State = EntityState.Modified;
+                        employee.firstName = firstNameEntered;
+                        employee.lastName = lastNameEntered;
+                        employee.jobTitle = jobTitleEntered;
+
+                    context.SaveChanges();
+                    Console.WriteLine("You have successfully updated employee data");
+            }
+
+            /**
+            * deleteEmpData function will prompt user which employee record to delete by obtaining empId
+            */
+            void deleteEmpData()
+            {
+                // delete employee data by employee id
+                Console.WriteLine("Enter Employee ID :");
+                int empIdEntered = Convert.ToInt32(Console.ReadLine());
+
+                    var employee = new Employee { empId = empIdEntered };
+                    context.Entry(employee).State = EntityState.Deleted;
+                    context.SaveChanges();
+                    Console.WriteLine("You have successfully deleted employee data");
+            }
+
             // provide options for user
             Console.WriteLine("What action would you like to perform: ADD, DELETE, or UPDATE");
             string actionSelected = Console.ReadLine();
                     if(actionSelected.ToLower() == "add")
                     {
-                        // add employee data
-                        Console.WriteLine("Enter First Name :");
-                        string firstNameEntered = Console.ReadLine();
-
-                        Console.WriteLine("Enter Last Name :");
-                        string lastNameEntered = Console.ReadLine();
-
-                        Console.WriteLine("Enter Job Title :");
-                        string jobTitleEntered = Console.ReadLine();
-
-                        var employee = new Employee {
-                            firstName = firstNameEntered,
-                            lastName = lastNameEntered,
-                            jobTitle = jobTitleEntered
-                        };
-                        context.Add(employee);
-                        context.SaveChanges();
-                        Console.WriteLine("You have successfully added employee data");
-
-                    } else if (actionSelected.ToLower() == "delete")
-                    {
-                        // delete employee data by employee id
-                        Console.WriteLine("Enter Employee ID :");
-                        int empIdEntered = Convert.ToInt32(Console.ReadLine());
-
-                        var employee = new Employee { empId = empIdEntered };
-                        context.Entry(employee).State = EntityState.Deleted;
-                        context.SaveChanges();
-                        Console.WriteLine("You have successfully deleted employee data");
-
+                        // call add function
+                        addEmpData();
                     } else if (actionSelected.ToLower() == "update")
                     {
-                        // update employee data
-                        Console.WriteLine("Enter Employee ID :");
-                        int empIdEntered = Convert.ToInt32(Console.ReadLine());
-
-                        Console.WriteLine("Enter Updated First Name :");
-                        string firstNameEntered = Console.ReadLine();
-
-                        Console.WriteLine("Enter Updated Last Name :");
-                        string lastNameEntered = Console.ReadLine();
-
-                        Console.WriteLine("Enter Updated Job Title :");
-                        string jobTitleEntered = Console.ReadLine();
-
-                        var employee = new Employee { empId = empIdEntered };
-                        context.Entry(employee).State = EntityState.Modified;
-
-                        employee.firstName = firstNameEntered;
-                        employee.lastName = lastNameEntered;
-                        employee.jobTitle = jobTitleEntered;
-
-                        context.SaveChanges();
-                        Console.WriteLine("You have successfully updated employee data");
+                        // call update function
+                        updateEmpData();
+                    } else if (actionSelected.ToLower() == "delete")
+                    {
+                        // call delete function
+                        deleteEmpData();
                     }
                     else
                     {
-                        Console.WriteLine("Please rerun the app following instructions");
+                        Console.WriteLine("Please rerun the app following correct instructions");
                     }
 
                 // display employee records
